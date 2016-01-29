bfaba90f9e9916c95af3da4d3fa6b440a618604c
Dylan Barnard <dylan.barnard@gmail.com>
2016-01-28 20:52:58 -0800

    Add coverage to exclude 8's and 9's

--- challenges/octal/octal.rb
+++ challenges/octal/octal.rb
@@ -4,12 +4,11 @@ class Octal
   end
 
   def to_decimal
-    return 0 unless @number.is_a?(String)
+    return 0 unless /^\d[^8|9]+$/ =~ @number
     @number.split('').reverse!.each_with_index.inject(0) do | sum, (num, idx)|
       sum += num.to_i * 8 ** idx
     end
   end
 end
-
 
 
