bd02b1bd743350f553d468da94083c4593dee98d
Dylan Barnard <dylan.barnard@gmail.com>
2016-01-28 19:21:27 -0800

    Refactor of Octal challenge

--- challenges/octal/octal.rb
+++ challenges/octal/octal.rb
@@ -3,23 +3,13 @@ class Octal
     @number = number
   end
 
-  def is_number?
+  def to_decimal
     return 0 unless @number.is_a?(String)
-  end
-
-  def transform
-    is_number?
-    result = 0
-    number_arr = @number.split('').reverse!
-    number_arr.each_with_index do |num, idx| 
-      result += num.to_i * 8 ** idx
+    @number.split('').reverse!.each_with_index.inject(0) do | sum, (num, idx)|
+      sum += num.to_i * 8 ** idx
     end
-    result
   end
 end
-
-oct = Octal.new('10')
-puts oct.transform
 
 
 
