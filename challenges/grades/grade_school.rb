2ae7fa7873ede501e73c57ed8294679473398b7a
dylankb <dylan.barnard@gmail.com>
2016-01-29 16:17:51 -0800

    Attempt at grade_school coding challenge

---
+++ challenges/grades/grade_school.rb
@@ -0,0 +1,24 @@
+class School
+  attr_accessor :school
+
+  def initialize
+    @school = {}
+  end
+
+  def add(name, grade)
+    new_student = {grade => [name]}
+    @school.merge!(new_student) {|key, oldval, newval| oldval << newval[0]}
+  end
+
+  def to_h
+    output = []
+    @school.each do |grade|
+      output << grade
+      yield(grade) if block_given?
+    end
+  end
+
+  def grade(grade)
+    @school.fetch(grade, Array.new) 
+  end
+end
