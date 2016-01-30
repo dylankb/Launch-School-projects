30ace3f85847b62c0c76267f5fc3fab0213998ab
dylankb <dylan.barnard@gmail.com>
2016-01-29 16:26:13 -0800

    Clean up ivars

--- challenges/grades/grade_school.rb
+++ challenges/grades/grade_school.rb
@@ -1,5 +1,5 @@
 class School
-  attr_accessor :school
+  attr_reader :school
 
   def initialize
     @school = {}
@@ -7,18 +7,18 @@ class School
 
   def add(name, grade)
     new_student = {grade => [name]}
-    @school.merge!(new_student) {|key, oldval, newval| oldval << newval[0]}
+    school.merge!(new_student) {|key, oldval, newval| oldval << newval[0]}
   end
 
   def to_h
     output = []
-    @school.each do |grade|
+    school.each do |grade|
       output << grade
       yield(grade) if block_given?
     end
   end
 
   def grade(grade)
-    @school.fetch(grade, Array.new) 
+    school.fetch(grade, Array.new) 
   end
 end
