5369d94324f8b3f911864d6fd42b8d66e573236c
dylankb <dylan.barnard@gmail.com>
2016-02-13 12:04:29 -0800

    Post review: Add last key to START_DAY hash to eliminate some lines conditional flow

--- challenges/meetup/meetup.rb
+++ challenges/meetup/meetup.rb
@@ -3,7 +3,12 @@ require 'date'
 class Meetup
   attr_reader :year, :month
 
-  START_DAY = { first: 1, second: 8, teenth: 13, third: 15, fourth: 22 }
+  START_DAY = { first: 1, 
+                second: 8, 
+                teenth: 13, 
+                third: 15, 
+                fourth: 22, 
+                last: -7 }
 
   def initialize(month, year)
     @month = month
@@ -15,17 +20,12 @@ class Meetup
   end
 
   def find_date_range(schedule)
-    if schedule == :last
-      last_day = Date.new(year, month, -1).day
-      7.times.inject([]) { |days, num| days << Date.new(year, month, last_day - num) }
-    else
-      7.times.inject([]) { |days, num| days << Date.new(year, month, START_DAY[schedule] + num) }
-    end
+    7.times.inject([]) { |days, num| days << Date.new(year, month, START_DAY[schedule] + num) }
   end
 
   def find_weekday_in_month(day, schedule)
     dates = find_date_range(schedule)
-    dates.select! { |date| date.public_send((day.to_s + '?').to_sym) } #i.e date.monday?
+    dates.select! { |date| date.public_send((day.to_s + '?').to_sym) }
     dates[0]
   end
 end
