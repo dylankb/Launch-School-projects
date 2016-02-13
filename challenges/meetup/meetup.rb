643a37d45a96cbef1f965f13d6bc65c2d08b2b91
dylankb <dylan.barnard@gmail.com>
2016-02-13 12:40:17 -0800

    Post review: move functionality to test method that was did nothing but call that other method

--- challenges/meetup/meetup.rb
+++ challenges/meetup/meetup.rb
@@ -16,17 +16,13 @@ class Meetup
     @year = year
   end
 
-  def day(weekday, schedule)
-    find_weekday_in_month(weekday, schedule)
+  def day(day, schedule)
+    dates = find_date_range(schedule)
+    dates.select! { |date| date.public_send((day.to_s + '?').to_sym) }
+    dates[0]
   end
 
   def find_date_range(schedule)
     7.times.inject([]) { |days, num| days << Date.new(year, month, START_DAY[schedule] + num) }
-  end
-
-  def find_weekday_in_month(day, schedule)
-    dates = find_date_range(schedule)
-    dates.select! { |date| date.public_send((day.to_s + '?').to_sym) }
-    dates[0]
   end
 end
