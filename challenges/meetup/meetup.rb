417bbf2671b26f56a513e2a58b6c87ed2efd1c08
dylankb <dylan.barnard@gmail.com>
2016-02-12 23:31:40 -0800

    Small naming changes

--- challenges/meetup/meetup.rb
+++ challenges/meetup/meetup.rb
@@ -17,15 +17,15 @@ class Meetup
   def find_date_range(schedule)
     if schedule == :last
       last_day = Date.new(year, month, -1).day
-      7.times.inject([]) { |acc, num| acc << Date.new(year, month, last_day - num) }
+      7.times.inject([]) { |days, num| days << Date.new(year, month, last_day - num) }
     else
-      7.times.inject([]) { |acc, num| acc << Date.new(year, month, START_DAY[schedule] + num) }
+      7.times.inject([]) { |days, num| days << Date.new(year, month, START_DAY[schedule] + num) }
     end
   end
 
   def find_weekday_in_month(day, schedule)
     dates = find_date_range(schedule)
-    dates.select! { |date| date.public_send((day.to_s + '?').to_sym) }
+    dates.select! { |date| date.public_send((day.to_s + '?').to_sym) } #i.e date.monday?
     dates[0]
   end
 end
