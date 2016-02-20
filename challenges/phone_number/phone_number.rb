62dc5ecd8d2a9e00ad34dcb6d7d97e03dec4e697
dylankb <dylan.barnard@gmail.com>
2016-02-20 12:16:31 -0800

    Remove unnecessary self calls on methods, add methods to make to_s logic cleaner

--- challenges/phone_number/phone_number.rb
+++ challenges/phone_number/phone_number.rb
@@ -5,20 +5,35 @@ class PhoneNumber
   end
 
   def number
-    return '0000000000' unless @number_string.scan(/[a-z]/).size == 0
+    return '0000000000' if has_alphabetic_characters?
     numbers = @number_string.scan(/\d/).join
     numbers.slice!(0,1) if numbers[0] == '1' && numbers.size == 11
     return '0000000000' unless numbers.size == 10
     numbers    
   end
 
+  def has_alphabetic_characters?
+    @number_string.scan(/[a-z]/).size != 0
+  end
+
   def area_code
-    self.number[0..2]
+    number[0..2]
+  end
+
+  def central_office_code
+    number.chars.slice(3..5).join
+  end
+
+  def line_code
+    number.chars.slice(6..9).join
   end
 
   def to_s
-    numbers = self.number.chars
-    "(#{self.area_code}) #{numbers.slice(3..5).join}-#{numbers.slice(6..9).join}"
+    "(#{area_code}) #{central_office_code}-#{line_code}"
   end
 end
+
+puts PhoneNumber.new('9876543210').to_s
+
+
 
