85b671f61728a152789fb326cd40930e0775c019
dylankb <dylan.barnard@gmail.com>
2016-03-01 14:46:24 -0800

    Read a file's content in plain text

--- 170/cms/cms.rb
+++ 170/cms/cms.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require "tilt/erubis"
+require 'pry'
 
 root = File.expand_path("..", __FILE__)
 
@@ -9,4 +10,12 @@ get "/" do
   @data = Dir.glob(root + '/data/*').map { |file| File.basename(file)}.sort
 
   erb :data
+end
+
+get "/data/:filename" do
+
+  file_path = root + "/data/" + params[:filename]
+  headers["Content-Type"] = "text/plain"
+  
+  File.read(file_path)
 end
