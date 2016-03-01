fe30d06ec5c420480a40457af1ba8d5fa40f2f11
dylankb <dylan.barnard@gmail.com>
2016-03-01 14:45:20 -0800

    Add root to filepath to make file fetching less error prone

--- 170/cms/cms.rb
+++ 170/cms/cms.rb
@@ -2,9 +2,11 @@ require 'sinatra'
 require 'sinatra/reloader'
 require "tilt/erubis"
 
+root = File.expand_path("..", __FILE__)
+
 get "/" do 
   "Getting started"
-  @data = Dir.glob('data/*').map { |file| File.basename(file)}.sort
+  @data = Dir.glob(root + '/data/*').map { |file| File.basename(file)}.sort
 
   erb :data
 end
