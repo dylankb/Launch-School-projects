1668dcd923bcff61f889962c07d0a56acf2ce6bc
dylankb <dylan.barnard@gmail.com>
2016-03-02 13:20:28 -0800

    1) Fix error messaging bug by actually setting up sessions before trying to use them 2) Add ability to render .md files

--- 170/cms/cms.rb
+++ 170/cms/cms.rb
@@ -1,9 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require "tilt/erubis"
-require 'pry'
+require "redcarpet"
 
 root = File.expand_path("..", __FILE__)
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+helpers do
+  def render_markdown(file)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown.render(file)
+  end
+end
 
 get "/" do
   "Getting started"
@@ -16,8 +28,13 @@ get "/:filename" do
   file_path = root + "/data/" + params[:filename]
 
   if File.exist?(file_path)
-    headers["Content-Type"] = "text/plain"
-    File.read(file_path)
+    if File.extname(file_path) == ".md"
+      file = File.read(file_path)
+      render_markdown(file)
+    else
+      headers["Content-Type"] = "text/plain"
+      File.read(file_path)
+    end
   else
     session[:message] = "#{params[:filename]} does not exist."
     redirect "/"
