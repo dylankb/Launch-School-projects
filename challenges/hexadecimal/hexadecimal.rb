e6196e0fa9ac45bfaa2ed488714a30bdda771a64
dylankb <dylan.barnard@gmail.com>
2016-03-22 10:00:45 -0700

    Complete hexadecimal challenge

--- challenges/hexadecimal/hexadecimal.rb
+++ challenges/hexadecimal/hexadecimal.rb
@@ -1,7 +1,7 @@
 class Hexadecimal
 
-numeric = (10..16).to_a
-alpha = ('a'...'f').to_a
+numeric = (10..15).to_a
+alpha = ('a'...'g').to_a
 ALPHANUMERIC = {}
 alpha.each_with_index do | letter, index|
   ALPHANUMERIC[letter] = numeric[index]
@@ -11,16 +11,18 @@ end
     @string = string
   end
 
-# use hash to covert to_i. run on all elements
   def format_values
     @string.chars.map do |character|
-      character =~ /\D/ ? ALPHANUMERIC[character.downcase] ? character.to_i
+      character =~ /\D/ ? ALPHANUMERIC[character.downcase] : character.to_i
     end
   end
 
   def to_decimal
-    format_values.reverse.map
+    format_values.reverse.map.with_index do |value, index|
+      value * (16 ** index)
+    end.inject(:+)
   end
 end
 
-p Hexadecimal.new('1e42').convert_to_array_of_integers
+#p Hexadecimal.new('1e42').to_decimal
+
