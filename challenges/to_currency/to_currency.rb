f6eac491c9e1fafc6d594c63fcff5af849851341
Dylan <dylan.barnard@gmail.com>
2016-04-17 12:53:44 -0700

    to_currency code challenge

---
+++ challenges/to_currency/to_currency.rb
@@ -0,0 +1,17 @@
+def to_currency(number_string)
+  raise "Invalid currency entry" if /[^\d|.]/ =~ number_string
+
+  dollars = number_string.split(".")[0]
+                         .reverse
+                         .chars
+                         .each_slice(3)
+                         .map do |interval|
+                            interval.reverse.join 
+                          end
+                          .reverse
+                          .join(',')
+
+  cents = sprintf("%.2f", number_string).scan(/\.\d+/)[0]
+
+  "$" + dollars + cents
+end
