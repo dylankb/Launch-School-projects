35e4717bf3b0e8d71c5bfa2b354e96f5fd98e8fa
dylankb <dylan.barnard@gmail.com>
2017-01-29 22:37:53 -0800

    Complete simple OOP exercise

---
+++ 120-oop/exercises/medium/01_privacy.rb
@@ -0,0 +1,21 @@
+class Machine
+
+  def start
+    flip_switch(:on)
+  end
+
+  def stop
+    flip_switch(:off)
+  end
+
+  private
+
+  attr_writer :switch
+
+  def flip_switch(desired_state)
+    self.switch = desired_state
+  end
+
+end
+
+puts Machine.new().flip_switch(:on)  # fails
