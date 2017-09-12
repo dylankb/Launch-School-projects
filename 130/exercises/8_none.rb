1b725807fa65c77777f1f16e4eee34f3ba7222a8
dylankb <dylan.barnard@gmail.com>
2017-09-12 11:13:30 -0700

    Small change to exercise

--- 130/exercises/8_none.rb
+++ 130/exercises/8_none.rb
@@ -4,15 +4,13 @@
 # end
 
 def any?(collection)
-  collection.each { |ele| return false unless yield(ele) }
-  true
+  collection.each { |ele| return true if yield(ele) }
+  return false
 end
 
 def none?(collection, &block)
   !any?(collection, &block)
 end
-
-
 
 # var = none?([1, 3, 5, 6]) { |value| value.even? } == false
 # p var
@@ -20,8 +18,9 @@ bar = none?([2, 4, 6, 8]) do |value|
   value.odd? == true
 end
 puts bar
-none?([2, 4, 6, 8]) { |value| value.odd? } == true
-none?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
-none?([1, 3, 5, 7]) { |value| true } == false
-none?([1, 3, 5, 7]) { |value| false } == true
-none?([]) { |value| true } == true
+puts none?([2, 4, 6, 8]) { |value| value.odd? }              # true
+puts none?([2, 4, 6, 8]) { |value| value.odd? } == true      # true
+puts none?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false # true
+puts none?([1, 3, 5, 7]) { |value| true } == false           # true
+puts none?([1, 3, 5, 7]) { |value| false } == true           # true
+puts none?([]) { |value| true } == true                      # true
