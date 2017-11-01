0c58660d03b427666b1fca3114ee3a18b1d404ce
dylankb <dylan.barnard@gmail.com>
2017-11-01 15:30:43 -0700

    Setup for SQLite3

--- 301-rails/blog/db/schema.rb
+++ 301-rails/blog/db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101183250) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20171101202029) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
