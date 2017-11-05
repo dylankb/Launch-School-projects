52f910974ec8eba42cffcd57a03acaad3167ef94
dylankb <dylan.barnard@gmail.com>
2017-11-05 12:35:16 -0800

    Try categories_posts HMT

--- 301-rails/postit/app/models/post.rb
+++ 301-rails/postit/app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
-  has_many :categorizations
-  has_many :categories, :through => :categorizations
+  has_many :categories_posts
+  has_many :categories, :through => :categories_posts
 end
--- 301-rails/postit/db/schema.rb
+++ 301-rails/postit/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104031313) do
+ActiveRecord::Schema.define(version: 20171106183125) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20171104031313) do
     t.datetime "updated_at"
   end
 
-  create_table "categorizations", force: true do |t|
+  create_table "categories_posts", force: true do |t|
     t.integer  "post_id"
     t.integer  "category_id"
     t.datetime "created_at"
