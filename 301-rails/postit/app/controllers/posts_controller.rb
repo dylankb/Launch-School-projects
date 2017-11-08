620b0920a0d40eb256130bcadb35f4d8e4923fee
dylankb <dylan.barnard@gmail.com>
2017-11-08 15:48:23 -0800

    Add update + edit actions for posts

--- 301-rails/postit/app/controllers/posts_controller.rb
+++ 301-rails/postit/app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
   
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -23,10 +24,24 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    if @post.update(post_params)
+      flash[:notice] = "Post updated"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:title, :url, :description)
   end
