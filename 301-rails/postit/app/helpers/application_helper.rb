e3137fe4a782261871fecdc14bbf28f2e8218533
dylankb <dylan.barnard@gmail.com>
2017-11-14 14:12:55 -0800

    Add datetime helper

--- 301-rails/postit/app/helpers/application_helper.rb
+++ 301-rails/postit/app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def format_url(url)
     url.starts_with?("http://") ? url : "http://#{url}"
   end
+  def format_date(date)
+    date.strftime("%m/%d/%Y %l:%M%P %Z") 
+  end
 end
