b440c8f43f8447191a00b3e760cb0862c4030ea8
dylankb <dylan.barnard@gmail.com>
2017-11-17 13:38:46 -0800

    Create user login + register forms, routes and validations

--- 301-rails/postit/config/routes.rb
+++ 301-rails/postit/config/routes.rb
@@ -1,5 +1,14 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
+
+  get '/register', to: 'users#new'
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  get '/logout', to: 'session#destroy'
+
+  resources :users, only: [:show, :create, :edit, :update]
+  resources :sessions
+
   resources :posts, except: :destroy do
     resources :comments, only: :create
     resources :categories, only: :create
