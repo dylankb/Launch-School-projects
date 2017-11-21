6ece866b2b26cf1cdfc8d63c6a43a231722b4625
dylankb <dylan.barnard@gmail.com>
2017-11-21 09:12:05 -0800

    Add navigation dropdown, ability to edit user profile

--- 301-rails/postit/app/controllers/users_controller.rb
+++ 301-rails/postit/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
@@ -14,6 +15,24 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Profile updated!"
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
   def user_params
