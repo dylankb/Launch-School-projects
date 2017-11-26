e30b3750a444b2c14fc212f08c57bb93a5a3bf6c
dylankb <dylan.barnard@gmail.com>
2017-11-25 17:25:24 -0800

    Create votes table and relationships
    - Votes has polymorphic relationship to comments and posts
    - Also standardize quotes

--- 301-rails/postit/app/models/post.rb
+++ 301-rails/postit/app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ActiveRecord::Base
-  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
+  belongs_to :creator, foreign_key: "user_id", class_name: "User"
   has_many :comments
   has_many :categorizations
-  has_many :categories, :through => :categorizations
+  has_many :categories, through: :categorizations
+  has_many :votes, as: :voteable
 
   validates :title, presence: true, length: { minimum: 5 }
   validates :url, presence: true
